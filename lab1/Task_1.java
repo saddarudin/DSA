4176f22ddb8fcdf7d9ba57c8d0b04c10b54d5f14
Saddar U Din <saddarudin338@gmail.com>
2022-12-26 15:57:19 +0500

    comments removed

---
+++ lab1/Task_1.java
@@ -0,0 +1,20 @@
+package lab1;
+
+
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int age=29;
+        System.out.println("Age: "+age);
+        boolean isAManager=true;
+        System.out.println("is a manager: "+isAManager);
+        double yearsOfService=2.5;
+        System.out.println("year of services: "+yearsOfService);
+        int baseSalary=3000;
+        int overTimePayment=40;
+        int totalPayment=baseSalary+overTimePayment;
+        System.out.println("Total Payment: "+totalPayment);
+        double firstBonus=10.00,secondBonus=22.00,thirdBonus=35.00;
+        System.out.println("Sum = "+(firstBonus+secondBonus+thirdBonus));
+    }
+}
